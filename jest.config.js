fe3bc40da7ecb732004997758d07510dfba8e0f2
steven <648800939@qq.com>
2022-10-14 15:16:33 +0800

    chore:tsconfig.build.json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
-const baseConfig = require('../../jest.config.js')
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
 
 module.exports = {
-  ...baseConfig,
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ['json', 'html'],
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  },
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
 }
