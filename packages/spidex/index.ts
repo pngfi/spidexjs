f0837a9655230157f2d1f26aa24b517b99a0ae44
steven <648800939@qq.com>
2022-10-24 20:51:26 +0800

    fix: spidex ->spidexjs

--- packages/spidex/index.ts
+++ packages/spidex/index.ts
@@ -1,6 +1,6 @@
-import * as addressConverter from '../address-converter'
-import * as eip712 from '../eip712'
-import * as proto from '../proto'
+import * as addressConverter from '@spidex/address-converter'
+import * as eip712 from '@spidex/eip712'
+import * as proto from '@spidex/proto'
 import * as provider from '../provider'
 import * as transactions from '../transactions'
 
