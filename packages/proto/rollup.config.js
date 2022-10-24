bc756f5f58fb03478a3de43a716edc436c604c95
steven <648800939@qq.com>
2022-10-24 18:37:24 +0800

    refact: update spidex package

---
+++ packages/proto/rollup.config.js
@@ -0,0 +1,48 @@
+
+import pkg from './package.json'
+import commonjs from 'rollup-plugin-commonjs' // commonjs模块转换插件
+import ts from 'rollup-plugin-typescript2'
+import json from "@rollup/plugin-json";
+import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const isProd = process.env.NODE_ENV === 'production';
+// plugins基础配置
+const plugins =[
+    peerDepsExternal({ includeDependencies: !isProd }),
+    commonjs(),
+    json(),
+    ts(
+      {
+        exclude: 'node_modules/**',
+      }
+    ),
+    terser
+]
+
+// 需要导出的模块类型
+const output =function(fileName){
+  return [
+    {
+      file: pkg.main, // 通用模块
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: pkg.main,// es6模块
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    }
+  ]
+}
+
+
+export default [
+  {
+    input: 'index.ts',
+    output: output(),
+    plugins:plugins
+  }
+]
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as addressConverter from './address-converter'
-import * as eip712 from './eip712'
-import * as proto from './proto'
-import * as provider from './provider'
-import * as transactions from './transactions'
-
-export { addressConverter, eip712, proto, provider, transactions }
